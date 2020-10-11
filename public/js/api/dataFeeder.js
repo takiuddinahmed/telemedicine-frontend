143defe5789ba0c3f041d6cac2932cad596db0c6
Al Fahad <fahad.ruet@gmail.com>
2020-10-11 20:02:35 +0600

    1

--- public/js/api/dataFeeder.js
+++ public/js/api/dataFeeder.js
@@ -8,3 +8,22 @@ $('#patientPregnancyStatus').val("yes");
 
 // Get doctor ID and send it to the outdoor API to get value
 
+$('#doctorName').text("Doctor Parvin Sultana");
+// $('#doctorBMDC');
+// $('#doctorCollege');
+// $('#doctorNumber');
+// $('doctorChemberContactNumber');
+
+$('#saveDoctorInfo').on('click',()=>{
+    console.log($('#doctorName').html());
+});
+
+$(()=>{
+    console.log("got here");
+    fetch('https://outdoorbd.com/rest-api/doctor/1/XDXTBDOPQQRX69FD')
+    .then(res=>res.json())
+    .then(resData=>console.log(resData))
+    .catch(err=>console.log(err));
+})
+
+
